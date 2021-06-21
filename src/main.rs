dd2cd738eaa01fd5b0fd81e28d8c51d898108bdf
Corentin Girard <corentingirard.dev@gmail.com>
2021-06-21 18:32:55 +0200

    Support stdin as input

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use regex::Regex;
 use std::env;
+use std::io::{self, BufRead};
 use std::process::exit;
 
-fn main() {
+fn get_input() -> Option<String> {
     let args: Vec<String> = env::args().collect();
-    let re = Regex::new(r"^\p{Extended_Pictographic}$").unwrap();
-
     if args.len() != 2 {
-        println!("Usage: is-emoji <arg>");
-        exit(128);
+        let stdin = io::stdin();
+        let line = stdin.lock().lines().next().unwrap().unwrap();
+        return Some(line);
     }
 
-    let is_emoji = re.is_match(&args[1]);
-    if !is_emoji {
-        exit(1);
+    Some(args[1].clone())
+}
+
+fn main() {
+    if let Some(input) = get_input() {
+        let re = Regex::new(r"^\p{Extended_Pictographic}$").unwrap();
+        let is_emoji = re.is_match(&input);
+        exit(!is_emoji as i32)
     }
+
+    println!("Usage: is-emoji <arg>");
+    exit(128);
 }
