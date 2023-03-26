da189ec9b05797ccb9c9a186a25b2ab42c33ba7f
corentin <corentin.girard@datadoghq.com>
2023-03-26 21:43:10 +0200

    Support compound emojis

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use std::env;
 use std::io::{self, BufRead};
 use std::process::exit;
 
-const EMOJI_REGEX: &str = r"^\p{Extended_Pictographic}$";
+// Note: https://unicode.org/reports/tr51/#Emoji_Properties
+
+// Taken from https://unicode.org/reports/tr51/#EBNF_and_Regex and adapted
+// to be future-proof with \p{Extended_Pictographic} instead of \p{Emoji}
+const EMOJI_REGEX: &str = r"^\p{Extended_Pictographic}(\p{EMod}|\x{FE0F}\x{20E3}?|[\x{E0020}-\x{E007E}]+\x{E007F})?(\x{200D}(\p{RI}\p{RI}|\p{Extended_Pictographic}(\p{EMod}|\x{FE0F}\x{20E3}?|[\x{E0020}-\x{E007E}]+\x{E007F})?))*$";
 
 fn get_input() -> Option<String> {
     if atty::is(Stream::Stdin) {
@@ -56,4 +60,38 @@ fn test_basic_emoji() {
     assert_eq!(is_emoji("😀"), true);
     assert_eq!(is_emoji("🥸"), true);
     assert_eq!(is_emoji("🔥"), true);
+}
+
+#[test]
+fn test_emoji_component() {
+    assert_eq!(is_emoji("🦲"), true); // bold
+    assert_eq!(is_emoji("🦱"), true); // curly hair
+    assert_eq!(is_emoji("🦳"), true); // white hair
+}
+
+#[test]
+fn test_compound_emoji() {
+    // https://unicode.org/reports/tr51/#def_emoji_zwj_sequence
+    assert_eq!(is_emoji("👨‍🚀"), true); // 👨 + ZWJ + 🚀
+    assert_eq!(is_emoji("❤‍🔥"), true); // ❤ + ZWJ + 🔥
+    assert_eq!(is_emoji("👨‍🦱"), true); // 👨 + ZWJ + 🦱
+
+    // https://unicode.org/reports/tr51/#Emoji_Variation_Sequences
+    assert_eq!(is_emoji("👁‍🗨"), true); // 👁️ + VS16 + ZWJ + 💬 + VS16
+}
+
+#[test]
+fn test_unicode_versions() {
+    // Unicode 12 [2019]
+    assert_eq!(is_emoji("🪑"), true);
+
+    // Unicode 13 [2020]
+    assert_eq!(is_emoji("🤌"), true);
+
+    // Unicode 14 [2021]
+    assert_eq!(is_emoji("🥹"), true);
+    assert_eq!(is_emoji("🫡"), true);
+
+    // Unicode 15 [2022] (not supported everywhere)
+    assert_eq!(is_emoji("🫨"), true); // shaking face (https://www.emojiall.com/en/code/1FAE8)
 }
